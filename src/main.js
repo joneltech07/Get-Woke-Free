75e9b8eab0f0a06e38183f306e034abc023aafdb
joneltech07 <jonel.uligan7@gmail.com>
2024-01-27 00:21:49 +0800

    CDN Removed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-
+import '../node_modules/jquery/dist/jquery.js';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
